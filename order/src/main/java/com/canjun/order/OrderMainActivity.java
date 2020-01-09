7a953ab1b46de121315b95ea7332d4ce01ad3b17
fengcheng.zhang@percent.cn <fengcheng.zhang@percent.cn>
2020-01-09 14:57:58 +0800

    分别创建了order personal common

---
+++ order/src/main/java/com/canjun/order/OrderMainActivity.java
@@ -0,0 +1,20 @@
+package com.canjun.order;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * OrderMainActivity
+ *
+ * @author zfc
+ * @date 2020-01-09
+ */
+public class OrderMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_main_activity);
+    }
+}
---
+++ personal/src/main/java/com/canjun/personal/PersonalMainActivity.java
@@ -0,0 +1,14 @@
+package com.canjun.personal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_activity_main);
+    }
+}
